abb0c3163af7efaa19f3826313f6d472724801be
Your Name <hp2393@gmail.com>
2019-04-30 12:44:31 -0400

    added prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,4 +25,24 @@ function start (){
             console.log(`Id: ${row.item_id} Name: ${row.product_name} Price: ${row.price}\n` )
         });
     })
-}
+};
+
+
+questions()
+function questions() {
+    inquirer.prompt([
+        {
+            message: "Type in the product ID to place an order/",
+            type: "input",
+            name: "productID"
+        },
+        {
+            message: "How many iteams would you like to purchase with us today?",
+            type: "input",
+            name: "productQTY"
+        }
+    ]).then(function (response) {
+        var productID = response.productID;
+        var productQTY = response.productQTY;
+    });
+};
